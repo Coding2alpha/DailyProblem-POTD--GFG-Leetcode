e58770dfd1f28b89ae7c92bc69f7fff8c2405c03
Coding2alpha <omsinghthakur2502@gmail.com>
2023-12-29 16:55:32 +0530

    29-12-23

---
+++ 28-12-23GFG.cpp
@@ -0,0 +1,50 @@
+class Solution{
+    public:
+    bool solve(int i,int j,string &s,string &p,int &n,int &m,vector<vector<int>> &dp)
+    {
+        if(i==n && j==m)
+        {
+            return true;
+        }
+        if(i==n && j<m)
+        {
+            for(int k=j;k<m;k++)
+            {
+                if(p[k]!='*')
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        if(j==m && i<n)
+        {
+            return false;
+        }
+        if(dp[i][j]!=-1)
+        {
+            return dp[i][j];
+        }
+        if(s[i]==p[j] || p[j]=='?')
+        {
+            return dp[i][j]=solve(i+1,j+1,s,p,n,m,dp);
+        }
+        // case for *
+        else  if(p[j]=='*')
+        {
+            return dp[i][j]=(solve(i,j+1,s,p,n,m,dp) || solve(i+1,j,s,p,n,m,dp));
+        }
+        // case when s[i]!=p[j]
+        else
+        {
+            return dp[i][j]=false;
+        }
+    }
+    bool match(string p, string s)
+    {
+           int n=s.size();
+       int m=p.size();
+       vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
+       return solve(0,0,s,p,n,m,dp);
+    }
+};
